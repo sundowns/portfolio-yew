cb3a4ae54d0e993ee36ca9998cb86650dcf7036c
Tom Smallridge <tom.smallridge@versent.com.au>
2021-01-04 13:12:44 +0800

    dependencies make brain hurt

--- src/components/content.rs
+++ src/components/content.rs
@@ -1,54 +1,47 @@
-use components::content::*;
-
-use wasm_bindgen::prelude::*;
-use yew::prelude::*;
-
-struct MainProject {
-    link: ComponentLink<Self>,
-    content: [ProjectContent; 4],
+pub enum ContentType {
+    Video,
+    Screenshot,
+}
+pub struct ProjectContent {
+    content_type: ContentType,
+    alt_text: String,
+    url: String,
 }
 
-enum Msg {}
+impl ProjectContent {
+    // Another static method, taking two arguments:
+    fn new(content_type: ContentType, alt_text: &str, url: &str) -> ProjectContent {
+        ProjectContent {
+            content_type: content_type,
+            alt_text: alt_text.to_owned(),
+            url: url.to_owned(),
+        }
+    }
+}
 
-impl Component for MainProject {
+pub struct VideoContentCard {
+    link: ComponentLink<Self>,
+}
+impl Component for VideoContentCard {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            content: [ProjectContent::new(
-                ContentType::Video,
-                "gaming1",
-                "https://i.gyazo.com/94297d4712add817da9ec7ecda9fa5a4.mp4",
-            )],
-        }
+        Self { link }
     }
-
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         false
     }
-
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
         // Should only return "true" if new properties are different to
         // previously received properties.
         // This component has no properties so we will always return "false".
         false
     }
-
     fn view(&self) -> Html {
         html! {
             <div>
-                // <ul>
-                //     <VideoContentCard/>
-                // </ul>
-                // TODO: use the screenshots field (iterate over it and display videos)
                 <a href="https://gyazo.com/94297d4712add817da9ec7ecda9fa5a4"><video alt="Video from Gyazo" width="640" autoplay=true muted=true loop=true playsinline=true controls=true><source src="https://i.gyazo.com/94297d4712add817da9ec7ecda9fa5a4.mp4" type="video/mp4" /></video></a>
             </div>
         }
     }
-}
-
-#[wasm_bindgen(start)]
-pub fn run_app() {
-    App::<MainProject>::new().mount_to_body();
 }
